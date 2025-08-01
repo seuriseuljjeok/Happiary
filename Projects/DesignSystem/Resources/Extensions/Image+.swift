3eb06d71225e9c183f5bc03cbb000b1a3f31c9d2
seuriseuljjeok <heeseul09@naver.com>
2025-08-01 16:10:00 +0900

    [Add] add image extension (#3)

---
+++ Projects/DesignSystem/Resources/Extensions/Image+.swift
@@ -0,0 +1,54 @@
+//
+//  Image+.swift
+//  DesignSystem
+//
+//  Created by 윤희슬 on 8/1/25.
+//
+
+import SwiftUI
+
+public extension Image {
+    
+    static let btnBack = Image(.btnBack)
+    
+    static var icArrowNext = Image(.icArrowNext)
+    
+    static var icArrowPrevious = Image(.icArrowPrevious)
+    
+    static var icRightArrow = Image(.icRightArrow)
+    
+    static var icPlus = Image(.icPlus)
+    
+    static var icCamera = Image(.icCamera)
+    
+    static var icCalendar = Image(.icCalendar)
+    
+    static var paper = Image(.paper)
+    
+    static var fresh = Image(.fresh)
+
+    static var peace = Image(.peace)
+
+    static var pitapat = Image(.pitapat)
+
+    static var sweat = Image(.sweat)
+
+    static var warm = Image(.warm)
+
+    static var selectedHome = Image(.selectedHome)
+
+    static var unselectedHome = Image(.unselectedHome)
+
+    static var selectedLog = Image(.selectedLog)
+
+    static var unselectedLog = Image(.unselectedLog)
+    
+    static var selectedArchive = Image(.selectedArchive)
+
+    static var unselectedArchive = Image(.unselectedArchive)
+
+    static var selectedSetting = Image(.selectedSetting)
+
+    static var unselectedSetting = Image(.unselectedSetting)
+
+}
