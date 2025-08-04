0461cb7a793bf29aa2435fb778d0b92b0535120d
seuriseuljjeok <heeseul09@naver.com>
2025-08-04 16:49:42 +0900

    [Feat] implement HappiaryTabBarItem (#3)

---
+++ Projects/DesignSystem/Resources/Components/HappiaryTabBarItem.swift
@@ -0,0 +1,52 @@
+//
+//  HappiaryTabBarItem.swift
+//  DesignSystem
+//
+//  Created by 윤희슬 on 8/4/25.
+//
+
+import SwiftUI
+
+
+public struct HappiaryTabBarItem: View {
+    
+    let title: String
+    
+    let icon: Image
+    
+    let isSelected: Bool
+    
+    let onTap: () -> Void
+    
+    public init(
+        title: String,
+        icon: Image,
+        isSelected: Bool,
+        onTap: @escaping () -> Void
+    ) {
+        self.title = title
+        self.icon = icon
+        self.isSelected = isSelected
+        self.onTap = onTap
+    }
+    
+    public var body: some View {
+        VStack(alignment: .center, spacing: 7) {
+            icon
+                .customImageStyle(size: 18)
+            
+            Text(title)
+                .customTextStyle(
+                    alignment: .center,
+                    fontSize: 12,
+                    fontColor: isSelected ? .accentCoral : .deepBrown
+                )
+        }
+        .frame(width: 75, height: 54)
+        .contentShape(Rectangle()) // expend tap area
+        .onTapGesture {
+            onTap()
+        }
+    }
+    
+}
