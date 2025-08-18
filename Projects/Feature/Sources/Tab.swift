d60b93f72141881acd0d6194ab49671dd9215cea
seuriseuljjeok <heeseul09@naver.com>
2025-08-18 17:20:57 +0900

    [Fix] move Tab-related code from Core module to the Sources of the Feature module (#5)

---
+++ Projects/Feature/Sources/Tab.swift
@@ -0,0 +1,73 @@
+//
+//  empty.swift
+//  Manifests
+//
+//  Created by 윤희슬 on 8/18/25.
+//
+
+
+import SwiftUI
+
+import DesignSystem
+
+
+public enum Tab: CaseIterable {
+    
+    case home
+    
+    case log
+    
+    case archive
+    
+    case setting
+    
+    public var title: String {
+        switch self {
+        case .home:
+            return "홈"
+            
+        case .log:
+            return "로그"
+
+        case .archive:
+            return "아카이브"
+
+        case .setting:
+            return "설정"
+
+        }
+    }
+    
+    public var unselectedIcon: Image {
+        switch self {
+        case .home:
+            return .unselectedHome
+            
+        case .log:
+            return .unselectedLog
+        
+        case .archive:
+            return .unselectedArchive
+            
+        case .setting:
+            return .unselectedSetting
+        }
+    }
+    
+    public var selectedIcon: Image {
+        switch self {
+        case .home:
+            return .selectedHome
+            
+        case .log:
+            return .selectedLog
+        
+        case .archive:
+            return .selectedArchive
+            
+        case .setting:
+            return .selectedSetting
+        }
+    }
+    
+}
